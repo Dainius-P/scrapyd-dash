c43cdf1ba67af1229b50bb2f812326a9b93beb3c
dainius <preimantasd@gmail.com>
2019-07-24 14:19:04 +0300

    static files

--- config/settings.py
+++ config/settings.py
@@ -122,8 +122,5 @@ USE_TZ = False
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "scrapyd_dash/static"),
-]
 
 django_heroku.settings(locals())
