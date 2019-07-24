ef58892dce00f77a4c0d03bdbb1f70881f0d8b3d
dainius <preimantasd@gmail.com>
2019-07-24 14:16:51 +0300

    static files

--- config/settings.py
+++ config/settings.py
@@ -123,7 +123,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "scrapyd_dash/static"),
     '/var/www/static/',
 ]
 
